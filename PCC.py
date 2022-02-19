cbdbc706700eff86141d7ac86fa7759f560faa26
MolecularNeurobiology <95201519+MolecularNeurobiology@users.noreply.github.com>
2022-02-18 18:40:24 -0600

    revert to prior release-001 version
    release-001 was accidentally updated - this should revert back to the release-001 version

--- PCC.py
+++ PCC.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = '42.0.1'
+__VERSION__ = '41.0.2'
 
 """
-
 Physiology Command Center
 (C) 2019
 @author: Christopher Ward (christow@bcm.edu, ward.chris.s@gmail.com)
-
 Created as part of the Russell Ray Molecular Neurobiology Group's
 Autoresuscitation Project
-
 contributions to this project include code, concepts, or consultation from 
 several individuals including Russell Ray, Eunice Aissi, Dipak Patel, 
 Mariana Garcia Costa, Savannah Lusk, Brandon Ruiz, and Kevin Jiang
-
-
 This software provides a graphical interface for I/O between an computer
 and 1) Arduino Microcontroller, 2) LabJack Analog to Digital Converter.
 Signals from the LabJack undergo signal processing to identify key features
 used as triggers to execute programmed control sequences run by the Arduino.
-
 The current implementation utilizes pneumotachography and electrocardiogram 
 signals to monitor breathing and heart rate as part of a neonate 
 autoresuscitation assay.
-
-
 Default Workflow (subject to change)
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 0-signal preview mode (adjust baseline and confirm tunable parameters)
@@ -52,16 +44,13 @@ Default Workflow (subject to change)
 6-experiment ended mode 
 **terminate preview and capture, send signal to notify user
 **signal to DC out, or other Raspberry Pi notification
-
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Inputs: currently none - all settings are coordinated within the GUI
 Outputs: timeseries signal datafile 
     [calibration capture, animal signal capture] - this is currently one file 
     with seperate sections
-
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 TODO regarding PCC
-
 *create flexibility for alternate study designs/data collection
 *create flexibility to terminate study after variable number of trials
 *error checking to prevent inversion of y axis, baseline and threshold values
@@ -70,25 +59,13 @@ TODO regarding PCC
     (probably try except and closing out connection)
 *better handling for arduino connection
 *incorporate logging library for improved status and debugging...maybe?
-
 *revisit UX restrictions for tweakables for display and general UX design
-
-
 related but slightly seperate
 *server/client for comms with Supervisor System and Worker Systems 
     (i.e. central workstation communicates to rigs running PCC for set-up and 
      monitoring)
 *tools to adapt PCC output for BASSPRO_STAGG pipeline, and Rice D2K pipelines
-
-!!! v42.0.0 goals CW !!!
-*check/fix CO2 timer
-*migrate settings to external file
-*add challenge endpoint based on trial number
-*start minor gui improvements
-*EXTERNAL CODE - PCC output converter
 """
-
-
 
 ##
 #%% import libraries
@@ -184,7 +161,6 @@ def guiGetFloat(title,text,default_if_canceled):
     .....
     declare as a dictionairy)
     {"title":"","minvalue":"","maxvalue":""}
-
     .....
     """
     root=tkinter.Tk().withdraw()
@@ -206,7 +182,6 @@ def guiGetText(title,text,default_if_canceled):
     .....
     declare as a dictionairy)
     {"title":"","minvalue":"","maxvalue":""}
-
     .....
     """
     root=tkinter.Tk().withdraw()
@@ -221,49 +196,6 @@ def guiGetText(title,text,default_if_canceled):
             root.destroy()
         except: pass
         return outputtext
-
-# class OptionPanel:
-#     def __init__(self,panel_title, option_dict):
-#         root = tkinter.Tk()
-#         root.title = panel_title
-#         self.Buttons = {}
-#         for k in option_dict:
-#             self.Buttons[k] = tkinter.Button(
-#                 root,
-#                 text = option_dict[k]['text'],
-#                 value = option_dict[k]['value'],
-#                 command = option_dict[k]['command'],
-#                 bg = option_dict[k]['bg_color'],
-#                 height = option_dict[k]['height'],
-#                 width = option_dict[k]['width']
-#                 )
-#             self.Buttons[k].grid(sticky ='S')
-    
-
-# #%% option panel test
-# TestPanel = OptionPanel(
-#     'this is just a test',
-#     {'button1':
-#      {
-#       'text':'button1',
-#       'command':guiGetText('test1','test1 text','default'),
-#       'bg_color':'green',
-#       'height':10,
-#       'width':40
-#       },
-#      'button2':
-#       {
-#        'text':'button2',
-#        'command':guiGetText('test2','test2 text','default'),
-#        'bg_color':'red',
-#        'height':10,
-#        'width':40
-#        }
-#      }
-#         )
-                        
-
-
 #%%
 def emailnotification(emailsettingslocation,dev):
     with open(emailsettingslocation,'r') as oif:
@@ -764,14 +696,13 @@ Mode_dict={0:'startup',
            2:'Signal Preview 1',
            3:'calibration',
            4:'Signal Preview 2',
-           5:'Habituation-1',
+           5:'Habituation',
            6:'Signal Preview 3',
            7:'Pre-Inject',
            8:'Inject',
-           9:'Habituation-2',
-           10:'Baseline',
-           11:'Challenge',
-           12:'Finished'}
+           9:'Baseline',
+           10:'Challenge',
+           11:'Finished'}
 Mode_timing={0:-1,
              1:-1,
              2:-1,
@@ -782,9 +713,8 @@ Mode_timing={0:-1,
              7:10*60,
              8:-1,
              9:15*60,
-             10:10*60,
-             11:-1,
-             12:-1}
+             10:-1,
+             11:-1}
 
 savable_modes=['calibration','Habituation','Pre-Inject','Baseline','Challenge']
 
@@ -904,11 +834,8 @@ Position_RA_xySize=(200,25)
 Position_Gas_xySize=(200,25)
 Duration_Cal_xySize=(200,25)
 Duration_Prefill_xySize=(200,25)
-Duration_Challenge_Delay_xySize=(200,25)
-Text_Challenge_Phrase_xySize=(200,25)
 
 Serial_Abort_xySize=(100,50)
-Serial_Rec_OR_xySize=(100,50)
 Serial_ShutDown_xySize=(100,50)
 finish_startup_xySize=(200,50)
 SerialOutTester_xySize=(200,50)
@@ -925,20 +852,14 @@ Arduino_Function_Constants={
     'Duration_Prefill':60
     }
 
-Challenge_phrase='Finished: On Anoxic'
-Challenge_Timer=datetime.now()
-Challenge_Delay=5
+Position_RA_TL=(700,250)
+Position_Gas_TL=(700,275)
+Duration_Cal_TL=(700,300)
+Duration_Prefill_TL=(700,325)
 
-Position_RA_TL=(700,200)
-Position_Gas_TL=(700,225)
-Duration_Cal_TL=(700,250)
-Duration_Prefill_TL=(700,275)
-Position_Challenge_Delay_TL=(700,300)
-Position_Challenge_Phrase_TL=(700,325)
 
-Serial_Abort_TL=(650,350)
-Serial_Rec_OR_TL=(750,350)
-Serial_ShutDown_TL=(850,350)
+Serial_Abort_TL=(700,350)
+Serial_ShutDown_TL=(800,350)
 finish_startup_TL=(700,400)
 
 SerialOutTester_TL=(700,450)
@@ -977,11 +898,11 @@ absthresh_ecg_TL=(40,700)
 thresh_ecg1_TL=(450,700)
 thresh_ecg2_TL=(450,770)
 
-INVERT_FLOW_TL=(200,880)
-PLETHFILT_TL=(200,905)
+INVERT_FLOW_TL=(950,300)
+PLETHFILT_TL=(950,325)
 
-ECGFILT_TL=(400,905)
-INVERT_ECG_TL=(400,880)
+ECGFILT_TL=(950,360)
+INVERT_ECG_TL=(950,385)#
 
 HR_recovery_thresh_TL=(950,420)
 BPM_recovery_thresh_TL=(950,450)
@@ -1535,10 +1456,10 @@ box_stream_lag=labeledbutton(BACKGROUND_COLOR,RED,
                              150,25,
                              '{:#.3F}'.format(stream_lag),
                                  (ScreenSize[0]-150,ScreenSize[1]-25))
-PLETHFILT_TOGGLE=labeledbutton(RED,BLACK,200,25,'FILT FLOW',PLETHFILT_TL)
-ECGFILT_TOGGLE=labeledbutton(RED,BLACK,200,25,'FILT ECG',ECGFILT_TL)
-INVERT_FLOW_TOGGLE=labeledbutton(WHITE,BLACK,200,25,'Inv Flow:{}'.format(INVERT_FLOW),INVERT_FLOW_TL)
-INVERT_ECG_TOGGLE=labeledbutton(WHITE,BLACK,200,25,'Inv ECG:{}'.format(INVERT_ECG),INVERT_ECG_TL)
+PLETHFILT_TOGGLE=labeledbutton(RED,BLACK,300,25,'FILTER PLETH',PLETHFILT_TL)
+ECGFILT_TOGGLE=labeledbutton(RED,BLACK,300,25,'FILTER ECG',ECGFILT_TL)
+INVERT_FLOW_TOGGLE=labeledbutton(WHITE,BLACK,300,25,'Invert Flow:{}'.format(INVERT_FLOW),INVERT_FLOW_TL)
+INVERT_ECG_TOGGLE=labeledbutton(WHITE,BLACK,300,25,'Invert ECG:{}'.format(INVERT_ECG),INVERT_ECG_TL)
 
 box_minimum_resus_time=labeledbutton(WHITE,BLACK,300,25,'RECOVERY:{:d}/{:d})'.format(int(current_recovery),int(minimum_resus_time)),minimum_resus_time_TL)
 
@@ -1610,17 +1531,9 @@ Duration_Cal=labeledbutton(BLACK,WHITE,Duration_Cal_xySize[0],Duration_Cal_xySiz
 Duration_Prefill=labeledbutton(BLACK,WHITE,Duration_Cal_xySize[0],Duration_Cal_xySize[1],
                                'Prefill dur: {}'.format(Arduino_Function_Constants['Duration_Prefill']),
                                Duration_Prefill_TL)
-Duration_Challenge_Delay=labeledbutton(BLACK,WHITE,Duration_Challenge_Delay_xySize[0],Duration_Challenge_Delay_xySize[1],
-                                       'Challenge Delay: {}'.format(Challenge_Delay),
-                                       Position_Challenge_Delay_TL)
-Text_Challenge_Phrase=labeledbutton(BLACK,WHITE,Text_Challenge_Phrase_xySize[0],Text_Challenge_Phrase_xySize[1],
-                                    'Phrase: {}'.format(Challenge_phrase),
-                                    Position_Challenge_Phrase_TL)
 
 Serial_Abort=labeledbutton(RED,BLACK,Serial_Abort_xySize[0],Serial_Abort_xySize[1],
                            'ABORT!',Serial_Abort_TL)
-Serial_Rec_OR=labeledbutton(GREEN,BLACK,Serial_Rec_OR_xySize[0],Serial_Rec_OR_xySize[1],
-                            'ORide!',Serial_Rec_OR_TL)
 Serial_ShutDown=labeledbutton(BLACK,WHITE,Serial_ShutDown_xySize[0],Serial_ShutDown_xySize[1],
                               'ShutDown',Serial_ShutDown_TL)
 
@@ -1809,14 +1722,11 @@ sprite_list.add(Position_RA)
 sprite_list.add(Position_Gas)
 sprite_list.add(Duration_Cal)
 sprite_list.add(Duration_Prefill)
-sprite_list.add(Duration_Challenge_Delay)
-sprite_list.add(Text_Challenge_Phrase)
 sprite_list.add(SLB_Trigger_Setter)
 sprite_list.add(Challenge_Counter)
 sprite_list.add(CurrentChallengeCO2_Timer)
 sprite_list.add(CurrentChallengeRecovery_Timer)
 sprite_list.add(Serial_Abort)
-sprite_list.add(Serial_Rec_OR)
 sprite_list.add(Serial_ShutDown)
 sprite_list.add(SerialOutTester)
 sprite_list.add(SO1)
@@ -1856,12 +1766,6 @@ Requests=0
 d = u6.U6()
 # For applying the proper calibration to readings.
 d.getCalibrationData()
-
-try:
-    d.streamStop()
-    print('stream found running - now stopped')
-except:
-    print('labjack pre-stream checked')
 ##
 
 #"""
@@ -1928,8 +1832,6 @@ ardThread.start()
 Arduino_Dump_Toggle=0
 Challenge_Toggle=0
 Challenge_phrase='Finished: On Anoxic'
-Challenge_Timer=datetime.now()
-Challenge_Delay=5
 
 try:
     while running==True: # the main game loop
@@ -1948,8 +1850,6 @@ try:
                 print(serial_list)
             for i in arduino_list:
                 if Challenge_phrase in i:
-                    if Challenge_Toggle==0:
-                        Challenge_Timer=datetime.now()
                     Challenge_Toggle=1
         #%% update state of program
         if sdr.finished==False:
@@ -1980,25 +1880,6 @@ try:
                     value_Challenge_Counter=1
                     value_CurrentChallengeCO2_Start=datetime.now()
                     print('first challenge')
-
-                elif (SinceLastBreath>=SLB_Trigger or Abort_Toggle==1) and cur_STATUS_Dict['challenge gas']==1:
-                    cur_STATUS_Dict['challenge air']=1
-                    cur_STATUS_Dict['challenge gas']=0
-                    Abort_Toggle=0
-                    slb_COLOR2=YELLOW
-                    value_CurrentChallengeRecovery_Start=datetime.now()
-                    
-                    print('{:#.2F} sec apnea detected'.format(SinceLastBreath))
-                    Challenge_Toggle=0
-                    print(value_CurrentChallenge_Timer)
-                    if value_CurrentChallenge_Timer<1+SLB_Trigger+Challenge_Delay:
-                        print('Recommend Update to Challenge Thresh!')
-                        serial_list+=['!!!','Warning - False Apnea Likely',
-                                      'Recommend Update to Threshold2',
-                                      '!!!']
-                        WarningColor=RED
-                        WarningText='[CLEAR]!!Check Threshold2!!'
-
                 #elif SinceLastBreath>=SLB_Trigger and cur_STATUS_Dict['challenge gas']==1 and numpy.average(r['AIN{}'.format(CHANNEL_DICT['BT'])])>1: #not sure why BT is being compared here...bad edit?
                 elif SinceLastBreath>=SLB_Trigger and cur_STATUS_Dict['challenge gas']==1:
                     cur_STATUS_Dict['challenge air']=1
@@ -2195,17 +2076,7 @@ try:
                         Arduino_Function_Constants['Duration_Prefill']
                         )
                     Duration_Prefill.update(BLACK,WHITE,'Prefill dur {}'.format(Arduino_Function_Constants['Duration_Prefill']))
-                elif Duration_Challenge_Delay.rect.collidepoint(event.pos):
-                    Challenge_Delay=guiGetFloat(
-                        'Challenge Delay',
-                        'Challenge Delay',
-                        Challenge_Delay
-                        )
-                    Duration_Challenge_Delay.update(BLACK,WHITE,'Challenge Delay: {}'.format(Challenge_Delay))
-                elif Text_Challenge_Phrase.rect.collidepoint(event.pos):
-                    Challenge_Delay=guiGetText('Challenge Signal Phrase','Challenge Signal Phrase',Challenge_phrase)
-                    
-                    Text_Challenge_Phrase.update(BLACK,WHITE,'Phrase: {}'.format(Challenge_phrase))
+
                 elif Serial_Abort.rect.collidepoint(event.pos):
                     serialtext='<Z,0,0>'
                     try:
@@ -2213,9 +2084,7 @@ try:
                         print('{} - sent'.format(serialtext))
                     except:
                         print('unable to transmit "{} "via serial io'.format(serialtext))
-                elif Serial_Rec_OR.rect.collidepoint(event.pos):
-                    Recovery_Override_Toggle=1
-                    print('override')
+
                 elif Serial_ShutDown.rect.collidepoint(event.pos):
                     serialtext='<D,0,0>'
                     try:
@@ -2487,19 +2356,9 @@ try:
         
         if cur_STATUS_Dict['challenge gas']==1:
             Challenge_Counter.update(WHITE,BLACK,'Challenge #: {}'.format(value_Challenge_Counter))
-            value_CurrentChallengeCO2_Timer_uncorrected=cur_time-value_CurrentChallengeCO2_Start
-            if Challenge_Toggle==0:
-                value_CurrentChallengeCO2_Timer=\
-                value_CurrentChallengeCO2_Timer_uncorrected.days*24*60*60+\
-                value_CurrentChallengeCO2_Timer_uncorrected.seconds-\
-                Arduino_Function_Constants['Duration_Prefill']
-            else:
-                value_CurrentChallengeCO2_Timer=\
-                    (cur_time-Challenge_Timer).days*24*60*60+\
-                    (cur_time-Challenge_Timer).seconds
-            
+            value_CurrentChallengeCO2_Timer=cur_time-value_CurrentChallengeCO2_Start
             CurrentChallengeCO2_Timer.update(WHITE,BLACK,'CO2: {} sec'.format(
-                value_CurrentChallengeCO2_Timer))
+                value_CurrentChallengeCO2_Timer.days*24*60*60+value_CurrentChallengeCO2_Timer.seconds))
 
 
         elif cur_STATUS_Dict['challenge air']==1:
@@ -2655,25 +2514,16 @@ try:
             ts1=[i/1000+20/1000 for i in range(int(round((REL_TIMER-5)*1000,3)),int(REL_TIMER*1000),20)] # this may need adjusting if frequency is changed
             ts3=[i/1000+1/1000 for i in range(int(round((REL_TIMER-2.5)*1000,3)),int(REL_TIMER*1000),2)]
             
-            #reset annotation marks to green
-            Annot_Color=GREEN
-
             if Challenge_Toggle==1:
-                Annot_Color=VIOLET
-                # depreciated requirement for thresh2 to have been crossed to utilize - too likely to have error of missing apnea
-                #if max(data1)>=thresh2_flow and (datetime.now()-Challenge_Timer).seconds>=Challenge_Delay:
-                if (datetime.now()-Challenge_Timer).seconds>=Challenge_Delay:
+                if max(data1)>=thresh2_flow:
                     Challenge_Toggle = 2
-                    print('violet stopped')
-                    print((datetime.now()-Challenge_Timer).seconds)
-                
+            
             if cur_STATUS_Dict['challenge gas']==1 and Challenge_Toggle==2: # !!! this will need to be replaced with gas verify variable from serial io
                 BreathCalls=basic_breathcall(data1,ts1,baseline_flow,thresh2_flow)
-                # change marks to red when thresh 2 in use
                 Annot_Color=RED
             else:
                 BreathCalls=basic_breathcall(data1,ts1,baseline_flow,thresh_flow)
-                #Annot_Color=GREEN
+                Annot_Color=GREEN
             if BreathCalls is None or len(BreathCalls)<2:
                 avgBPM='<12'
                 #avgPIF='-----'
@@ -2877,11 +2727,7 @@ try:
             box_qual_bouts.update(YELLOW,BLACK,'bouts:{}'.format(QB_Counter))
             box_qual_dur.update(YELLOW,BLACK,'duration:{:#.1F}'.format(QB_duration))                   
             # updated unused boxes for demo video
-            try:
-                box_CT.update(WHITE,BLUE,'RT:{:#.1F}C|Pi:{:#.1F}C'.format(CT_value,CPUTemperature().temperature)) #see note abot regarding labjack internal temp
-            except:
-                # print('unable to get RPi CPU Temp - or other error, expected if testing on device other than RPi')
-                box_CT.update(WHITE,BLUE,'RT:{:#.1F}C|Pi:{}'.format(CT_value,'unk')) #see note abot regarding labjack internal temp
+            box_CT.update(WHITE,BLUE,'RT:{:#.1F}C|Pi:{:#.1F}C'.format(CT_value,CPUTemperature().temperature)) #see note abot regarding labjack internal temp
             box_BT.update(RED,BLACK,'CT: {:#.1F}C'.format(1000*numpy.average(r['AIN{}'.format(CHANNEL_DICT['BT'])])))
             box_RH.update(BLACK,BLACK,'RH: {:#.2F}V'.format(numpy.average(r['AIN{}'.format(CHANNEL_DICT['RH'])])))
             box_O2.update(BLACK,BLACK,'O2: {:#.2F}V'.format(numpy.average(r['AIN{}'.format(CHANNEL_DICT['O2'])])))
@@ -2980,7 +2826,13 @@ try:
 except Exception as e:
     print(e)
     traceback.print_exc()
+    # Close the device
+    d.setDIOState(0,0)
+    d.setDIOState(1,0)
+    d.setDIOState(2,0)
+    d.setDIOState(3,0)
 
+d.close()
 print('exit received')
 #% Wait for the stream thread to stop
 try:
@@ -2989,14 +2841,6 @@ try:
 except:
     print('no loose thread found')
 # Close the device
-try:
-    d.streamStop()
-    print('stream stopped')
-except:
-    print('no remaining stream found')
-    
-
-
 d.setDIOState(0,0)
 d.setDIOState(1,0)
 d.setDIOState(2,0)
