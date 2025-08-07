ef8d279fe7248d83f8a4760a6ae9fa6d3154ed39
Chris Ward <ward.chris.s@gmail.com>
2025-08-06 19:07:50 -0500

    add shell template for running GEMouse.py

---
+++ run_GEMouse.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# bash script to run gemouse
+
+# check for dependency 
+# swig - is a recently needed dependency for 2025 updates 
+# to autoresuscitation repository
+
+
+if ! command -v swig &> /dev/null
+then
+{
+  echo "Please install swig and try again. >sudo apt install swig"
+  exit 1
+}
+else
+{
+  echo "swig installed"
+}
+fi
+
+if ! dpkg -s liblgpio-dev &> /dev/null
+then
+{
+  echo "Please install liblgpio-dev"
+}
+else
+{
+  echo "liblgpio-dev found"
+}
+fi
+
+if ! command -v uv &> /dev/null
+then
+{
+  echo "Please install uv and try again. >curl -LsSf https://astral.sh/uv/install.sh | sh"
+}
+else
+{
+  echo "uv is installed"
+  cd $HOME/git/Autoresuscitation
+  uv run $HOME/git/Autoresuscitation/GEMouse.py &
+  uv run $HOME/git/Autoresuscitation/PCC.py
+}
+fi
