5ad1231b123ff0deeb83292d410885d2979a0555
Chris Ward-Rene <ward.chris.s@gmail.com>
2025-12-22 16:49:25 -0600

    rsync

--- ansible/source/Rsync_v3.sh
+++ ansible/source/Rsync_v3.sh
@@ -2,7 +2,7 @@
 
 # ==== IMPROVED RSYNC SCRIPT WITH NETWORK STABILITY CHECKS ====
 today=$(date +"%y%m%d")
-RIGNAME="Riggy"  # Change this value to set the rig name
+RIGNAME="Change My Name"  # Change this value to set the rig name
 source_dir="/media/pi/${RIGNAME}/"
 destination_dir="/home/pi/data2/Projects"
 
@@ -293,4 +293,4 @@ main() {
 }
 
 # Run the main function
-main "$@" (base)
+main "$@"
